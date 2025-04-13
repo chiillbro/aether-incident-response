e679e22c224cc28a3b948e14c4fc90865de10c78
Siva Sankar Reddy <aasamsivasankarreddynani143@gmail.com>
2025-04-13 21:54:00 +0530

    feat: checking GitHub actions - CI

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-   <div className="flex items-center justify-center h-screen font-bold text-2xl">
+   <div className="flex items-center justify-center h-screen font-bold text-2xl text-red-500">
     Hello World
    </div>
   );
